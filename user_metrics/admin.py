86747df610a871d0773182af575314d9de3604c9
Edu Burgoa Serrada <edu@playfilm.tv>
2016-01-28 14:48:36 +0100

    added some useful admin filters

--- user_metrics/admin.py
+++ user_metrics/admin.py
@@ -2,6 +2,71 @@ from django.contrib import admin
 
 from user_metrics.models import Metric, MetricItem, MetricDay, MetricWeek, MetricMonth, MetricQuarter, MetricYear
 from user_metrics.utils import get_quarter_number
+from django.contrib.admin.filters import DateFieldListFilter, AllValuesFieldListFilter, SimpleListFilter
+
+
+
+from datetime import date
+
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+class YearListFilter(SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('Month')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'date_up'
+
+    def lookups(self, request, model_admin):
+        available_dates = model_admin.model.objects.all().order_by('date_up').values('date_up').distinct()
+        lookup = []
+        for date in available_dates:
+            converted_date = (str(date["date_up"].year), _(str(date["date_up"].year)))
+            lookup.append(converted_date)
+        tuple(lookup)
+        return lookup
+
+    def queryset(self, request, queryset):
+        if self.value():
+            year = int(self.value())
+            next_year = year + 1
+            return queryset.filter(date_up__gte=date(int(year), 1, 1), date_up__lt=date(next_year, 1, 1))
+        else:
+            return queryset
+
+
+class MonthListFilter(SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('Month')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'date_up'
+
+    def lookups(self, request, model_admin):
+        available_dates = model_admin.model.objects.all().order_by('date_up').values('date_up').distinct()
+        lookup = []
+        for date in available_dates:
+            converted_date = (str(date["date_up"].month) + "_" + str(date["date_up"].year), _(date["date_up"].strftime("%B") + "/" + str(date["date_up"].year)))
+            lookup.append(converted_date)
+        tuple(lookup)
+        return lookup
+
+    def queryset(self, request, queryset):
+        if self.value():
+            m, y = self.value().split('_')
+            month, year = int(m), int(y)
+            if month < 12:
+                next_month = month + 1
+                next_year = year
+            else:
+                next_month = 1
+                next_year = year + 1
+            return queryset.filter(date_up__gte=date(int(year), int(month), 1), date_up__lt=date(next_year, next_month, 1))
+        else:
+            return queryset
 
 
 class MetricAdmin(admin.ModelAdmin):
@@ -15,9 +80,9 @@ class MetricItemAdmin(admin.ModelAdmin):
 
 class MetricDayAdmin(admin.ModelAdmin):
     list_display = ('metric', 'user', 'count', 'when', )
-    list_filter = ('metric', 'user', )
+    list_filter = (MonthListFilter, 'user', 'metric',)
 
-    search_fields = ['metric', 'user__username', 'date_up']
+    search_fields = ['metric', 'user__username']
 
     def when(self, obj):
         return obj.date_up.strftime("%b. %d, %Y")
@@ -27,7 +92,7 @@ class MetricDayAdmin(admin.ModelAdmin):
 
 class MetricWeekAdmin(admin.ModelAdmin):
     list_display = ('metric', 'user', 'count', 'when', )
-    list_filter = ('metric', 'user', )
+    list_filter = (MonthListFilter, 'user', 'metric',)
 
     search_fields = ['metric', 'user__username', 'date_up']
 
@@ -42,9 +107,9 @@ class MetricWeekAdmin(admin.ModelAdmin):
 
 class MetricMonthAdmin(admin.ModelAdmin):
     list_display = ('metric', 'user', 'count', 'when', )
-    list_filter = ('metric', 'user', )
+    list_filter = (MonthListFilter, 'user', 'metric',)
 
-    search_fields = ['metric', 'user__username', 'date_up']
+    search_fields = ['metric', 'user__username']
 
     def when(self, obj):
         return obj.date_up.strftime("%B %Y"),
@@ -55,7 +120,7 @@ class MetricMonthAdmin(admin.ModelAdmin):
 
 class MetricQuarterAdmin(admin.ModelAdmin):
     list_display = ('metric', 'user', 'count', 'when', )
-    list_filter = ('metric', 'user', )
+    list_filter = (YearListFilter, 'user', 'metric',)
 
     search_fields = ['metric', 'user__username', 'date_up']
 
@@ -71,7 +136,7 @@ class MetricQuarterAdmin(admin.ModelAdmin):
 
 class MetricYearAdmin(admin.ModelAdmin):
     list_display = ('metric', 'user', 'count', 'when', )
-    list_filter = ('metric', 'user', )
+    list_filter = (YearListFilter, 'user', 'metric',)
 
     search_fields = ['metric', 'user__username', 'date_up']
 

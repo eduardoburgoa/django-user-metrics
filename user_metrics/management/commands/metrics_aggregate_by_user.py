455e1e6b182f7acca8aaf2425554de63147f79c4
Edu Burgoa Serrada <edu@playfilm.tv>
2016-01-28 15:00:24 +0100

    PEP8 styling

--- user_metrics/management/commands/metrics_aggregate_by_user.py
+++ user_metrics/management/commands/metrics_aggregate_by_user.py
@@ -16,9 +16,9 @@ class Command(NoArgsCommand):
         for item in items:
             # Daily Aggregation
             metric_day, create = MetricDay.objects.get_or_create(
-                metric = item.metric,
-                user = item.user,
-                date_up = item.date_up
+                metric=item.metric,
+                user=item.user,
+                date_up=item.date_up
             )
             metric_day.count = metric_day.count + item.count
             metric_day.save()
@@ -26,9 +26,9 @@ class Command(NoArgsCommand):
             # Weekly Aggregation
             week_date = week_for_date(item.date_up)
             metric_week, create = MetricWeek.objects.get_or_create(
-                metric = item.metric,
-                user = item.user,
-                date_up = week_date
+                metric=item.metric,
+                user=item.user,
+                date_up=week_date
             )
             metric_week.count = metric_week.count + item.count
             metric_week.save()
@@ -36,9 +36,9 @@ class Command(NoArgsCommand):
             # Monthly aggregation
             month_date = month_for_date(item.date_up)
             metric_month, create = MetricMonth.objects.get_or_create(
-                metric = item.metric,
-                user = item.user,
-                date_up = month_date
+                metric=item.metric,
+                user=item.user,
+                date_up=month_date
             )
             metric_month.count = metric_month.count + item.count
             metric_month.save()
@@ -46,9 +46,9 @@ class Command(NoArgsCommand):
             # Quarter aggregation
             quarter_date = quarter_for_date(item.date_up)
             metric_quarter, create = MetricQuarter.objects.get_or_create(
-                metric = item.metric,
-                user = item.user,
-                date_up = quarter_date
+                metric=item.metric,
+                user=item.user,
+                date_up=quarter_date
             )
             metric_quarter.count = metric_quarter.count + item.count
             metric_quarter.save()
@@ -56,9 +56,9 @@ class Command(NoArgsCommand):
             # Yearly aggregation
             year_date = year_for_date(item.date_up)
             metric_year, create = MetricYear.objects.get_or_create(
-                metric = item.metric,
-                user = item.user,
-                date_up = year_date
+                metric=item.metric,
+                user=item.user,
+                date_up=year_date
             )
             metric_year.count = metric_year.count + item.count
             metric_year.save()

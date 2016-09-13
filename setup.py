e911767da11d38c56f1abd0a86ff0a0f1cb1bddc
Edu Burgoa Serrada <edu@playfilm.tv>
2016-09-13 18:52:47 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "django-user-metrics",
-    version = "0.2",
-    description = "capture metrics for each user",
-    long_description = read('README.rst'),
-    url = 'https://github.com/rmaceissoft/django-user-metrics',
-    author = 'Reiner Marquez',
-    author_email = 'rmaceissoft@gmail.com',
-    packages = find_packages(exclude=['tests', 'example', 'docs']),
-    classifiers = [
+    name="django-user-metrics",
+    version="0.2",
+    description="capture metrics for each user",
+    long_description="""django app that allows capture application metrics by each user individually, so after you can generate reports with aggregation of results
+by day or by week for each user""",
+    url='https://github.com/rmaceissoft/django-user-metrics',
+    author='Reiner Marquez',
+    author_email='rmaceissoft@gmail.com',
+    packages=find_packages(exclude=['tests', 'example', 'docs']),
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Framework :: Django',

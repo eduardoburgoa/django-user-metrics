d381e694b92d8f0722d52a504a56e1120a5179f8
Edu Burgoa Serrada <edu@playfilm.tv>
2016-09-13 17:41:01 +0200

    cleaning unused imports

--- user_metrics/management/commands/metrics_aggregate_by_user.py
+++ user_metrics/management/commands/metrics_aggregate_by_user.py
@@ -1,9 +1,6 @@
 import datetime
 from django.core.management.base import NoArgsCommand
 
-from user_metrics.models import Metric, MetricItem, MetricDay, MetricWeek, MetricMonth, MetricQuarter, MetricYear
-from user_metrics.utils import week_for_date, month_for_date, quarter_for_date, year_for_date
-
 from user_metrics.api import metrics_aggregate_by_user
 
 class Command(NoArgsCommand):
